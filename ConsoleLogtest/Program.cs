6d68de0e0cb1012640c981a88798f5de785d3765
nicwas <nickolas.waskiewicz@extendhealth.com>
2018-08-06 15:50:42 -0700

    Add Demo console app

---
+++ ConsoleLogtest/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Config;
+using NLog.Extensions.Logging;
+using System;
+
+namespace ConsoleLogtest
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var servicesProvider = BuildDi();
+            var runner = servicesProvider.GetRequiredService<Runner>();
+
+            runner.DoAction("Action1");
+
+            Console.WriteLine("Press ANY key to exit");
+            Console.ReadLine();
+
+            // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+            LogManager.Shutdown();
+        }
+
+        private static IServiceProvider BuildDi()
+        {
+            var services = new ServiceCollection();
+
+            //Runner is the custom class
+            services.AddTransient<Runner>();
+
+            services.AddSingleton<ILoggerFactory, LoggerFactory>();
+            services.AddSingleton(typeof(ILogger<>), typeof(Logger<>));
+            services.AddLogging((builder) => builder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace));
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+
+            //configure NLog
+            loggerFactory.AddNLog(new NLogProviderOptions { CaptureMessageTemplates = true, CaptureMessageProperties = true });
+            LogManager.LoadConfiguration("nlog.config");
+
+            return serviceProvider;
+        }
+    }
+}
